48fab5a26f39d3fbcc536642beeacb08ee50b632
MirjanaTomic <mirjanatomic@yahoo.com>
2024-04-29 12:27:14 +0200

    Added hard coded filtering to product-list component

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { EMPTY, catchError } from 'rxjs';
+import { EMPTY, catchError, filter, map } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
 
 import { Product } from './product';
@@ -15,12 +15,23 @@ export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories: ProductCategory[] = [];
+  selectedCategoryId = 1;
 
   products$ = this.productService.products$.pipe(
     catchError((err) => {
       this.errorMessage = err;
       return EMPTY;
     })
+  );
+
+  productsSimpleFilter$ = this.productService.products$.pipe(
+    map((products: Product[]) =>
+      products.filter((product: Product) =>
+        this.selectedCategoryId
+          ? product.categoryId === this.selectedCategoryId
+          : true
+      )
+    )
   );
 
   constructor(private productService: ProductService) {}
